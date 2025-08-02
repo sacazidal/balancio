d139fe048afdc0a115a0b955b36c08e2352072f4
darkness <tw1nkle001@yandex.ru>
2025-08-03 00:58:51 +0700

    установил зависимости, настроил и подключился к postgres через prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { inter } from "@/lib/fonts";
+import ToggleTheme from "@/components/toggle-theme";
+import { colorBackground } from "@/utils/color-vars";
 
 export const metadata: Metadata = {
   title: "Balancio",
@@ -16,14 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" suppressHydrationWarning>
-      <body className={`${inter.className} antialiased`}>
+      <body className={`${inter.className} antialiased ${colorBackground}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <main>{children}</main>
+          <ToggleTheme />
         </ThemeProvider>
       </body>
     </html>
