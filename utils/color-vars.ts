1ada4e5e13cfeea22190957f90e73c271e316ed8
darkness <tw1nkle001@yandex.ru>
2025-08-04 23:54:00 +0700

    переменные палтры цветов

--- utils/color-vars.ts
+++ utils/color-vars.ts
@@ -1,9 +1,9 @@
 export const colorBackground = "bg-slate-50 dark:bg-slate-900";
 export const colorContentCard = "bg-white dark:bg-slate-800";
-export const colorText = "text-slate-800 dark:text-slate-100";
-export const colorAccent = "text-amber-500 dark:text-amber-400"; //--dark:text-emerald-400-- --dark:text-sky-400-- --dark:text-violet-400--
+export const colorText = "text-slate-600 dark:text-slate-400";
+export const colorAccent = "text-amber-500 dark:text-amber-400";
 export const colorSquirrels = "text-emerald-500";
 export const colorFats = "text-sky-500";
 export const colorCarbohydrates = "text-violet-500";
 export const colorButton =
-  "bg-slate-700 hover:slate-800 dark:text-slate-200 dark:bg-amber-600";
+  "bg-slate-700 hover:bg-slate-600 dark:text-slate-200 dark:bg-amber-600 dark:hover:bg-amber-700";
