16ae66ed30cca730724498ff34cc885b632706f8
darkness <tw1nkle001@yandex.ru>
2025-08-04 23:56:57 +0700

    HeroSection и Subtitle на Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <></>;
+import HeroSection from "@/components/HeroSection";
+import Subtitle from "@/components/Subtitle";
+
+export default async function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Subtitle />
+    </>
+  );
 }
